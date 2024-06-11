8a226ad8dc98f71762d12a258610a5007163a864
Daniel Sharjil Huq <DanielSHuq@proton.me>
2024-06-11 20:54:41 +0800

    bot init

---
+++ index.js
@@ -0,0 +1,54 @@
+import "dotenv/config";
+import express from "express";
+import {
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes,
+} from "discord-interactions";
+import {
+  VerifyDiscordRequest,
+  getRandomEmoji,
+  DiscordRequest,
+} from "./utils.js";
+import { getShuffledOptions, getResult } from "./game.js";
+
+// Create an express app
+
+const app = express();
+// Get port, or default to 3000
+const PORT = 3000;
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+// Store for in-progress games. In production, you'd want to use a DB
+const activeGames = {};
+
+app.post("/interactions", async function (req, res) {
+  const { type, id, data } = req.body;
+
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" command
+    if (name === "test") {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: "hello world " + getRandomEmoji(),
+        },
+      });
+    }
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
